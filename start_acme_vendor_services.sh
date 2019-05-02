9d58ae860816b5c231da6c9d2b7689a72061c6f0
vbelfoure <vbelfoure@hotmail.com>
2019-05-02 16:49:38 -0600

    initial commit

---
+++ start_acme_vendor_services.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+while getopts ":hi" opt; do
+  case ${opt} in
+    h )
+      echo "Usage:"
+      echo "-h          Display this message"
+      echo "-i          Initialize new database"
+      exit 0
+      ;;
+    i )
+      docker volume rm mongodata
+      ;;
+  esac
+done
+
+# Create mongodata volume.  Will do nothing if volume exists
+docker volume create mongodata
+
+# start application stack
+docker stack deploy -c .\docker-compose.yml acme
